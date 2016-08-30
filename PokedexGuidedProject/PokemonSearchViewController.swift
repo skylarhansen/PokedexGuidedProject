a489003d1138f2b1fa9420375b771bafbc2c8be0
skylarhansen <skylarshansen@gmail.com>
2016-08-30 12:01:55 -0600

    added views in storyboard/made view controller

---
+++ PokedexGuidedProject/PokemonSearchViewController.swift
@@ -0,0 +1,40 @@
+//
+//  PokemonSearchViewController.swift
+//  PokedexGuidedProject
+//
+//  Created by Skylar Hansen on 8/30/16.
+//  Copyright © 2016 Skylar Hansen. All rights reserved.
+//
+
+import UIKit
+
+class PokemonSearchViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var abilitiesLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
